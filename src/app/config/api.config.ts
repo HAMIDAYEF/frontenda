6f33638bc566ef5efb73427a554483d5d6e1af04
meriem <yefsahhamida1@gmail.com>
2026-05-21 16:36:28 +0200

    update

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  apiUrl: 'https://driving-school-backend-mbpw.onrender.com/',
+  apiUrl: 'https://driving-school-backend-mbpw.onrender.com',
 };
 /* apiUrl: 'https://infinity-prod.com/beta/napolitana/super-admin-api/',*/
