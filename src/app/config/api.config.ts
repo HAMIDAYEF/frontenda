73b523715f269df79335ee755588861b9acf2326
meriem <yefsahhamida1@gmail.com>
2026-05-22 20:29:34 +0200

    Increase Angular build budget

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  apiUrl: 'http://localhost:3500/api',
+  apiUrl: 'https://driving-school-backend-mbpw.onrender.com/api',
 };
 /* apiUrl: 'https://infinity-prod.com/beta/napolitana/super-admin-api/',*/
